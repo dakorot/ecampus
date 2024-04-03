0742cb9b975ea1b9fdc94f6937a6adbf821599eb
dakorot <koropenko.daria@gmail.com>
2024-04-03 21:46:58 +0200

    Login and Logout

--- ecampus/base/urls.py
+++ ecampus/base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
     path('', views.home_page, name='home'),
     # path('profile/<str:pk>', views.profile_page, name='profile'),
     path('feed/', views.feed_page, name='feed'),
--- ecampus/base/views.py
+++ ecampus/base/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+
+    context = {}
+    return render(request, 'base/login_form.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
 
 
 def home_page(request):
